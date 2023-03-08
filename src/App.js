154ceefe080ea26f07b07e193745d510d548e518
rohith_hundekar <rohith.hundekar@infomagnus.com>
2023-03-08 19:46:37 +0530

    folder changes+card component changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import ExpenseItem from "./components/ExpenseItem";
+import React from "react";
 import "./components/ExpenseItem.css";
+import Expenses from "./components/Expenses";
 
 
 function App() {
@@ -26,14 +27,9 @@ function App() {
   ];
 
   return (
-    <div className="expenses">
-      {/* <h2>Let's get started!</h2> */}
-      {/* <p>This is also visible!</p> */}
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
-    </div>
+   <div>
+    <Expenses items={expenses}/>
+   </div>
   );
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,19 @@
-import './ExpenseItem.css';
 import ExpenseDate from './ExpenseItemDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
 function ExpenseItem(props) {
      
 
   return (
     // <h2>Expense Component</h2>
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
       </div>
       <div className='expense-item__price'>$ {props.amount}</div>
-    </div>
+    </Card>
   );
 }
 
