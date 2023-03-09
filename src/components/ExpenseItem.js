7ba349611fc841641367efc7f4b6f97e5d0f23d6
rohith_hundekar <rohith.hundekar@infomagnus.com>
2023-03-09 18:35:08 +0530

    expense component changes

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,24 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseItemDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
-     
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title)
+  const clickHandler = () => {
+    setTitle('Updated!');
+    console.log('Clicked', title )}
 
   return (
     // <h2>Expense Component</h2>
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className='expense-item__price'>$ {props.amount}</div>
+      <button onClick={clickHandler}>Change Title </button>
     </Card>
   );
 }
